98bdc06b48e70e0dae7b8a1c63567c1888c352c4
nuigurumi03 <nuigurumi03@gmail.com>
2020-02-02 15:57:25 +0900

    自動デプロイの環境構築(Capistrano)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  server '18.176.241.79', user: 'ec2-user', roles: %w{app db web}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
